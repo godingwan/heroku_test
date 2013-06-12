139aa7193537e39947c26624a4c26fbc31d73185
Joshua Lee <jsungholee@gmail.com>
2013-06-12 12:53:34 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuTest::Application.config.secret_token = '3d83bfd577502e13d48991d3d39ae89f6781e02d407b0740b3c4e2ccebc4ca4e0ac93370bd846812735a0ebeb7e5654d83d15300739a1065d44e0596920ec9f0'
